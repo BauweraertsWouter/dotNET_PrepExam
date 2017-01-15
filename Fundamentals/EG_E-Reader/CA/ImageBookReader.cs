04f1982721bbbea2436cd188affd625789c9fa64
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-01-15 15:32:10 +0100

    SRP implemented: moved replaced INavigatable logic with IReader, implemented by TextBookReader and ImageBookReader

---
+++ Fundamentals/EG_E-Reader/CA/ImageBookReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA
+{
+    class ImageBookReader : IReader
+    {
+        private ImageBook Book { get; set; }
+        private int currentPage;
+
+        public ImageBookReader(ImageBook book)
+        {
+            this.Book = book;
+        }
+
+        public object Previous()
+        {
+            if (currentPage > 1 && currentPage <= Book.Images.Count())
+                currentPage--;
+            else
+                currentPage = 1;
+
+            return GetPage(currentPage);
+        }
+
+        public object Next()
+        {
+            if (currentPage > 0 && currentPage < Book.Images.Count())
+                currentPage++;
+            else
+                currentPage = 1;
+            return GetPage(currentPage);
+        }
+
+        private object GetPage(int currentPage)
+        {
+            return Book.Images.ElementAt(currentPage - 1);
+        }
+    }
+}
---
+++ Fundamentals/EG_E-Reader/CA/TextBookReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA
+{
+    class TextBookReader : IReader
+    {
+        private int currentPage;
+        private int pageSize = 35;
+
+        private TextBook Book {  get; set; }
+
+        public TextBookReader(TextBook book)
+        {
+            this.Book = book;
+        }
+
+        public object Previous()
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+            }
+            else currentPage = 1;
+
+            return GetPage(currentPage);
+        }
+
+        public object Next()
+        {
+            double numberOfPages = Math.Ceiling((double)Book.Content.Length / pageSize);
+            if ((double)currentPage < numberOfPages) currentPage++;
+            else currentPage = 1;
+
+            return GetPage(currentPage);
+        }
+
+        private object GetPage(int currentPage)
+        {
+            int remainingTextSize = Book.Content.Length - (pageSize * (currentPage - 1));
+            if (remainingTextSize < 0) remainingTextSize = 0;
+
+            return Book.Content.Substring((currentPage - 1) * pageSize, Math.Min(pageSize, remainingTextSize));
+        }
+    }
+}

8ca1faf7a8006de0f14f06d32744874332427314
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-01-15 15:55:05 +0100

    Finished EG_E-Reader

---
+++ Fundamentals/EG_E-Reader/CA/BookReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA
+{
+    class BookReader : IReader
+    {
+        private delegate object Navigate();
+
+        private Navigate previousNavHandler;
+        private Navigate nextNavHandler;
+
+        public BookReader(Book bookToRead)
+        {
+            this.LoadBook(bookToRead);
+        }
+
+        private void LoadBook(Book bookToRead)
+        {
+            IReader reader = null;
+
+            TextBook textBook = bookToRead as TextBook;
+            if (textBook != null)
+                reader = new TextBookReader(textBook);
+            else
+            {
+                ImageBook imageBook = bookToRead as ImageBook;
+                if (imageBook != null)
+                {
+                    reader = new ImageBookReader(imageBook);
+                }
+            }
+
+            if (reader != null)
+            {
+                previousNavHandler = reader.Previous;
+                nextNavHandler = reader.Next;
+            }
+            else throw new InvalidCastException("Unable to load the book");
+        }
+
+        public object Previous()
+        {
+            return previousNavHandler;
+        }
+
+        public object Next()
+        {
+            return nextNavHandler;
+        }
+    }
+}
